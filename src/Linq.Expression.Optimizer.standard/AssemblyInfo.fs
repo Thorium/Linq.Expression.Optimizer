d6c0d6f74de328c82a62297ff6e1ad8b7964d6f5
Tuomas Hietanen <thorium@iki.fi>
2025-06-04 13:34:37 +0100

    minor 3rd party updates

--- src/Linq.Expression.Optimizer.standard/AssemblyInfo.fs
+++ src/Linq.Expression.Optimizer.standard/AssemblyInfo.fs
@@ -5,13 +5,13 @@ open System.Reflection
 [<assembly: AssemblyTitleAttribute("Linq.Expression.Optimizer.standard")>]
 [<assembly: AssemblyProductAttribute("Linq.Expression.Optimizer")>]
 [<assembly: AssemblyDescriptionAttribute("Lightweight optimizer of System.Linq.Expression expressions. Just basic boolean algebra and reductions, constant and tuple/anonymous type eliminations.")>]
-[<assembly: AssemblyVersionAttribute("1.0.29")>]
-[<assembly: AssemblyFileVersionAttribute("1.0.29")>]
+[<assembly: AssemblyVersionAttribute("1.0.30")>]
+[<assembly: AssemblyFileVersionAttribute("1.0.30")>]
 do ()
 
 module internal AssemblyVersionInformation =
     let [<Literal>] AssemblyTitle = "Linq.Expression.Optimizer.standard"
     let [<Literal>] AssemblyProduct = "Linq.Expression.Optimizer"
     let [<Literal>] AssemblyDescription = "Lightweight optimizer of System.Linq.Expression expressions. Just basic boolean algebra and reductions, constant and tuple/anonymous type eliminations."
-    let [<Literal>] AssemblyVersion = "1.0.29"
-    let [<Literal>] AssemblyFileVersion = "1.0.29"
+    let [<Literal>] AssemblyVersion = "1.0.30"
+    let [<Literal>] AssemblyFileVersion = "1.0.30"
